a890dbd794f7505c95969d7a423f5ef830a164ea
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-23 22:55:33 -0700

    setup script for vagrant provisioner

--- Vagrantfile
+++ Vagrantfile
@@ -7,4 +7,21 @@ Vagrant::Config.run do |config|
   if local_bosh_src = ENV['BOSH_SRC']
     config.vm.share_folder "bosh-src", "/bosh", local_bosh_src
   end
+  
+  config.vm.provision :shell, :path => "setup.sh"
+  
+  if ENV['MULTIVM']
+    multivm_total = ENV['MULTIVM'].to_i
+    multivm_total = 4 if multivm_total < 4
+    # create VMs with internal IPs:
+    # - 192.168.11
+    # - 192.168.12
+    # - 192.168.13
+    # - 192.168.14
+    1.upto(4) do |n|
+      config.vm.define "rel#{n}".to_sym do |riak_config|
+        riak_config.vm.network :hostonly, "192.168.1.1#{n}"
+      end
+    end
+  end
 end
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e # exit immediately if a simple command exits with a non-zero status
+set -u # report the usage of uninitialized variables
+set +x
+
+if [[ ! -f /var/vcap/install_dependencies_complete ]]
+then
+  echo Installing dependencies for bosh-solo
+  # 
+  sudo apt-get install curl git-core -y
+  
+  if [[ ! -x /opt/sm/bin/sm ]]
+  then
+    if [[ ! -f /tmp/smf.sh ]]
+    then
+      curl -s -L https://get.smf.sh -o /tmp/smf.sh
+    fi
+    sudo chmod +x /tmp/smf.sh
+    sudo sh /tmp/smf.sh
+  fi
+  sudo /opt/sm/bin/sm ext install bosh-solo git://github.com/drnic/bosh-solo.git
+  sudo /opt/sm/bin/sm bosh-solo install_dependencies
+  # 
+  # source /etc/profile.d/rvm.sh
+  # sudo rvm 1.9.3 --default
+  # 
+  # sudo touch /var/vcap/install_dependencies_complete
+fi
