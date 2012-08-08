6c75cf5a281f41b7232b51987c8104b57f0f4f3a
Dr Nic Williams <drnicwilliams@gmail.com>
2012-08-08 10:25:30 -0600

    using bosh-solo vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -2,12 +2,12 @@ Vagrant::Config.run do |config|
   config.vm.box = "lucid64"
   config.vm.box_url = "http://files.vagrantup.com/lucid64.box"
 
+  config.vm.provision :shell, :path => "scripts/vagrant-setup.sh"
+
   if local_bosh_src = ENV['BOSH_SRC']
     config.vm.share_folder "bosh-src", "/bosh", local_bosh_src
   end
-  
-  config.vm.provision :shell, :path => "setup.sh"
-  
+
   if ENV['MULTIVM']
     multivm_total = ENV['MULTIVM'].to_i
     multivm_total = 4 if multivm_total < 4
--- scripts/vagrant-setup.sh
+++ scripts/vagrant-setup.sh
@@ -4,10 +4,17 @@ set -e # exit immediately if a simple command exits with a non-zero status
 set -u # report the usage of uninitialized variables
 set +x
 
-if [[ ! -f /var/vcap/install_dependencies_complete ]]
+if [[ ! -f ~/install_dependencies_complete ]]
 then
   echo Installing dependencies for bosh-solo
 
+  if [[ ! -f /etc/resolv.conf.bak ]]
+  then
+    mv /etc/resolv.conf /etc/resolv.conf.bak
+    echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+    echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
+  fi
+  
   sudo apt-get install curl git-core -y
   
   if [[ ! -x /opt/sm/bin/sm ]]
@@ -22,15 +29,7 @@ then
   sudo /opt/sm/bin/sm ext install bosh-solo git://github.com/drnic/bosh-solo.git
   sudo /opt/sm/bin/sm bosh-solo install_dependencies
 
-  sudo /usr/local/rvm/bin/rvm 1.9.3 --default
-  
-  mv /etc/resolv.conf /etc/resolv.conf.bak
-  echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
-  echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
-  
-  sudo mkdir -p /var/vcap
-  sudo touch /var/vcap/install_dependencies_complete
-  
+  touch ~/install_dependencies_complete
 fi
 
 # TODO run: sm bosh-solo update examples/dev-solo.yml or rel.yml
